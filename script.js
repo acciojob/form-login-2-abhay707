5b8d07c3d0132e530f29e9021f94b2f12e7be276
Abhay Chaturvedi <109450662+abhay707@users.noreply.github.com>
2026-07-09 13:50:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+function getFormvalue() {
+	
+	let fname = document.getElementById("fname").value;
+	let lname = document.getElementById("lname").value;
+	let phone = document.getElementById("phone").value;
+	let email = document.getElementById("email").value;
+	
+	alert(`First Name: ${fname} Last Name: ${lname} Phone Number: ${phone} Email ID: ${email}`);
+
+}
